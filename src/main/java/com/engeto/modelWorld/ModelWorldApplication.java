98d301e65d8b3d54fc24163323ebbbdb164b2820
tomas_mokry <tomas.mokry@gmail.com>
2023-04-24 15:05:38 +0200

    initial commit

---
+++ src/main/java/com/engeto/modelWorld/ModelWorldApplication.java
@@ -0,0 +1,13 @@
+package com.engeto.modelWorld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModelWorldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/engeto/modelWorld/ModelWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.engeto.modelWorld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModelWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
